f2b6f917d7c29346b9fc8d493a15b942386d432e
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-04-17 00:56:46 +0800

    update 02

--- 01_Introduction/02_Characters.c
+++ 01_Introduction/02_Characters.c
@@ -11,6 +11,18 @@ int main(){
     scanf("%c",&ch);
     printf("%c\n", ch);
 
+    /*  
+        Purpose:
+         This effectively "flushes" any remaining characters 
+         including the newline from the input buffer, 
+         ensuring that the next scanf starts fresh 
+         with new input from the user.
+
+        1. Calls getchar() repeatedly, 
+         which reads and discards one character at a time
+        
+        2. Continues until it finds a newline character (\n)
+    */
     while(getchar() != '\n');
 
     char s[100];
@@ -18,6 +30,15 @@ int main(){
     printf("%s\n",s);
     
     char sen[100];
+    /*
+        Purpose:
+         This is a safety feature to prevent buffer overflow
+
+        %99: 
+         It's a field width specifier that 
+         limits the number of characters that 
+         can be read into the sen array.
+    */
     scanf("%99[^\n]%*c", sen);
     printf("%s", sen);
     
